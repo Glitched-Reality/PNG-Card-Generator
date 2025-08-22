5c0ddef271ed393f463231612e7f480293783ec3
Glitched-Reality <me@glitchedreality.com>
2025-08-22 12:11:09 -0400

    Default Config Creating

--- main.py
+++ main.py
@@ -5,7 +5,38 @@ import textwrap
 import os
 import yaml
 
-with open("card_config.yaml", "r") as f:
+CONFIG_PATH = "card_config.yaml"
+
+DEFAULT_CONFIG = {
+    "border_styles": {
+        "Basic": [200, 200, 200, 80, 80, 80],
+        "Uncommon": [180, 255, 180, 0, 150, 0],
+        "Rare": [180, 200, 255, 0, 80, 200],
+        "Epic": [230, 200, 255, 100, 0, 100],
+        "Legendary": [255, 230, 180, 180, 100, 0]
+    },
+    "gradient_types": {
+        "Attack": [150, 0, 0, 255, 150, 150],
+        "Defense": [0, 0, 150, 150, 150, 255],
+        "Neutral": [80, 80, 80, 220, 220, 220],
+        "Bounty": [150, 120, 0, 255, 230, 150]
+    },
+    "card_size": {
+        "height": 900,
+        "width": 600
+    },
+    "border_width": 10,
+    "corner_radius": 25,
+    "font_file": "C:/Windows/Fonts/arial.ttf",
+    "font_title": 40,
+    "font_desc": 28
+}
+if not os.path.exists(CONFIG_PATH):
+    with open(CONFIG_PATH, "w") as f:
+        yaml.dump(DEFAULT_CONFIG, f)
+    print("Default config file created.")
+
+with open(CONFIG_PATH, "r") as f:
     config = yaml.safe_load(f)
 
 BORDER_STYLES = {
